ea5357cfe2b2368605ceb86cd9f1213b59e9aa6c
Ewelina <ewelinajazurek@gmail.com>
2021-10-14 20:52:59 +0200

    driverweb test

---
+++ cypress/integration/webdriver/test.js
@@ -0,0 +1,25 @@
+
+/// <reference types="Cypress" />
+
+describe("Test Contact Us form via WebdriverUni", () => {
+    it("Should be able to submit a successful submission via contact us form", () => {
+        // cypress code 
+        cy.visit("https://webdriveruniversity.com/Contact-Us/contactus.html")
+        // cy.get('#contact-us').click({force: true})
+        cy.get('[name ="first_name"]').type("Joe")
+        cy.get('[name ="last_name"]').type("Smith")
+        cy.get('[name ="email"]').type("joe@example.com")
+        cy.get('textarea.feedback-input').type("hello")
+        cy.get('[type="submit"]').click();
+    });
+
+    // .only to keep only 1 test scenario 
+    it.only("Should not be able to submit a successful submission via contact us form as all fields are required", () => {
+        cy.visit("https://webdriveruniversity.com/Contact-Us/contactus.html")
+        // cypress code 
+        cy.get('[name ="first_name"]').type("Tom")
+        cy.get('[name ="last_name"]').type("Smith")
+        cy.get('textarea.feedback-input').type("hello")
+        cy.get('[type="submit"]').click();
+    });
+})
