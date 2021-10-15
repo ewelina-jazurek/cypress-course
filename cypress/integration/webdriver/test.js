ee6e8388cce20b1dd4ede79e82ff02cb7c0817ce
Ewelina <ewelinajazurek@gmail.com>
2021-10-15 09:57:24 +0200

    cy.document title url

--- cypress/integration/webdriver/test.js
+++ cypress/integration/webdriver/test.js
@@ -5,6 +5,9 @@ describe("Test Contact Us form via WebdriverUni", () => {
     it("Should be able to submit a successful submission via contact us form", () => {
         // cypress code 
         cy.visit("https://webdriveruniversity.com/Contact-Us/contactus.html")
+        cy.document().should('have.property', 'charset').and('eq', 'UTF-8')
+        cy.title().should('include', 'WebDriver | Contact Us')
+        cy.url().should('include', 'contactus')
         // cy.get('#contact-us').click({force: true})
         cy.get('[name ="first_name"]').type("Joe")
         cy.get('[name ="last_name"]').type("Smith")
