8226a33e3965b6cabfa70bdb4e5fd1ad797e9f55
Ewelina <ewelinajazurek@gmail.com>
2021-10-15 09:27:09 +0200

    should 'have.attr' assertion

--- cypress/integration/webdriver/test.js
+++ cypress/integration/webdriver/test.js
@@ -9,17 +9,21 @@ describe("Test Contact Us form via WebdriverUni", () => {
         cy.get('[name ="first_name"]').type("Joe")
         cy.get('[name ="last_name"]').type("Smith")
         cy.get('[name ="email"]').type("joe@example.com")
+        cy.get('[name ="email"]').should('have.attr', 'name', 'email')
         cy.get('textarea.feedback-input').type("hello")
         cy.get('[type="submit"]').click();
+        cy.get('h1').should('have.text', 'Thank You for your Message!')
+
     });
 
     // .only to keep only 1 test scenario 
-    it.only("Should not be able to submit a successful submission via contact us form as all fields are required", () => {
+    it("Should not be able to submit a successful submission via contact us form as all fields are required", () => {
         cy.visit("https://webdriveruniversity.com/Contact-Us/contactus.html")
         // cypress code 
         cy.get('[name ="first_name"]').type("Tom")
         cy.get('[name ="last_name"]').type("Smith")
         cy.get('textarea.feedback-input').type("hello")
         cy.get('[type="submit"]').click();
+        cy.get('body').contains('Error: all fields are required')
     });
 })
