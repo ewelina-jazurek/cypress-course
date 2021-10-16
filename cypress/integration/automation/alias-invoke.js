8cae3f54a5793ff94836b870cd35f3e61a5a7f76
Ewelina <ewelinajazurek@gmail.com>
2021-10-16 13:11:28 +0200

    calculate total

--- cypress/integration/automation/alias-invoke.js
+++ cypress/integration/automation/alias-invoke.js
@@ -29,12 +29,36 @@ describe("Alias and invoke", () => {
         // });
 
         cy.get('.thumbnail').find('.oneprice').invoke('text').as('itemPrice')
+        cy.get('.thumbnail').find('.pricenew').invoke('text').as('saleItemPrice')
+
+
+        var itemsTotal = 0;
         cy.get('@itemPrice').then($linkText => {
+            var itemsPriceTotal = 0;
             var itemPrice = $linkText.split('$');
             var i;
             for (i = 0; i < itemPrice.length; i++) {
                 cy.log(itemPrice[i])
+                itemsPriceTotal += Number(itemPrice[i])
             }
+            itemsTotal += itemsPriceTotal;
+            cy.log("Non sale price items total: " + itemsPriceTotal)
         })
+
+        cy.get('@saleItemPrice').then($linkText => {
+            var saleItemsPriceTotal = 0
+            var saleItemPrice = $linkText.split('$');
+            var i;
+            for (i = 0; i < saleItemPrice.length; i++) {
+                cy.log(saleItemPrice[i])
+                saleItemsPriceTotal += Number(saleItemPrice[i])
+            }
+            itemsTotal += saleItemsPriceTotal;
+            cy.log("Sale price items total: " + saleItemsPriceTotal)
+        })
+            .then(() => {
+                cy.log("The total price of all products: " + itemsTotal)
+                expect(itemsTotal).to.equal(654.1)
+            })
     });
 });
