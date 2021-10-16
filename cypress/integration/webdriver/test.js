96697cacefd3335d0bd12fbc761e979b830f88dc
Ewelina <ewelinajazurek@gmail.com>
2021-10-16 13:22:19 +0200

    'removeAttr'

--- cypress/integration/webdriver/test.js
+++ cypress/integration/webdriver/test.js
@@ -2,9 +2,11 @@
 /// <reference types="Cypress" />
 
 describe("Test Contact Us form via WebdriverUni", () => {
-    it("Should be able to submit a successful submission via contact us form", () => {
+    it.only("Should be able to submit a successful submission via contact us form", () => {
         // cypress code 
-        cy.visit("https://webdriveruniversity.com/Contact-Us/contactus.html")
+        // cy.visit("https://webdriveruniversity.com/Contact-Us/contactus.html")
+        cy.visit("https://webdriveruniversity.com")
+        cy.get('#contact-us').invoke('removeAttr', 'target').click({ force: true })
         cy.document().should('have.property', 'charset').and('eq', 'UTF-8')
         cy.title().should('include', 'WebDriver | Contact Us')
         cy.url().should('include', 'contactus')
@@ -21,8 +23,9 @@ describe("Test Contact Us form via WebdriverUni", () => {
 
     // .only to keep only 1 test scenario 
     it("Should not be able to submit a successful submission via contact us form as all fields are required", () => {
-        cy.visit("https://webdriveruniversity.com/Contact-Us/contactus.html")
-        // cypress code 
+        // cy.visit("https://webdriveruniversity.com/Contact-Us/contactus.html")
+        cy.visit("https://webdriveruniversity.com")
+        cy.get('#contact-us').invoke('removeAttr', 'target').click({ force: true })
         cy.get('[name ="first_name"]').type("Tom")
         cy.get('[name ="last_name"]').type("Smith")
         cy.get('textarea.feedback-input').type("hello")
