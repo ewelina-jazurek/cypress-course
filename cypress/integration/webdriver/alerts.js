9f9d87379a4c61ee89de0093097a637bf07c42c3
Ewelina <ewelinajazurek@gmail.com>
2021-10-17 21:25:07 +0200

    expect(stub.getCall(0)).to.be.calledWith

--- cypress/integration/webdriver/alerts.js
+++ cypress/integration/webdriver/alerts.js
@@ -14,17 +14,45 @@ describe("Handle js alerts WebdriverUni", () => {
         });
     });
 
-    it.only("Validate js confirm alert box works correctly when clicking OK", () => {
+    it("Validate js confirm alert box works correctly when clicking OK", () => {
         // cy.visit("https://webdriveruniversity.com/Contact-Us/contactus.html")
         cy.visit("https://webdriveruniversity.com")
         cy.get('#popup-alerts').invoke('removeAttr', 'target').click({ force: true })
 
         cy.get('#button4').click()
 
-        cy.on('window:alert', (str) => {
+        cy.on('window:confirm', (str) => {
             return true
         });
         cy.get('#confirm-alert-text').contains('You pressed OK!')
+
+    });
+
+    it("Validate js confirm alert box works usin a stub", () => {
+        // cy.visit("https://webdriveruniversity.com/Contact-Us/contactus.html")
+        cy.visit("https://webdriveruniversity.com")
+        cy.get('#popup-alerts').invoke('removeAttr', 'target').click({ force: true })
+
+        cy.get('#button4').click()
+
+        cy.on('window:confirm', (str) => {
+            return false;
+        });
+        cy.get('#confirm-alert-text').contains('You pressed Cancel!')
+
+    });
+
+    it.only("Validate js confirm alert box works correctly when clicking Cancel", () => {
+        // cy.visit("https://webdriveruniversity.com/Contact-Us/contactus.html")
+        cy.visit("https://webdriveruniversity.com")
+        cy.get('#popup-alerts').invoke('removeAttr', 'target').click({ force: true })
+
+        const stub = cy.stub()
+        cy.on('window:confirm', stub)
+
+        cy.get('#button4').click().then(() => {
+            expect(stub.getCall(0)).to.be.calledWith('Press a button!')
+        })
 
     });
 })
