6d9af3d6e789df532a28df518c54a0569ae3c91e
Ewelina <ewelinajazurek@gmail.com>
2021-10-18 09:53:03 +0200

    @iframe

---
+++ cypress/integration/webdriver/iframe.js
@@ -0,0 +1,25 @@
+
+/// <reference types="Cypress" />
+
+describe("Handling iFrame & Modals", () => {
+    it.only("Handle webdriveruni iFrame & Modals", () => {
+
+        cy.visit("https://webdriveruniversity.com")
+        cy.get('#iframe').invoke('removeAttr', 'target').click({ force: true })
+        cy.get('#frame').then($iframe => {
+            const body = $iframe.contents().find('body')
+            cy.wrap(body).as('iframe')
+        })
+        cy.get('@iframe').find('#button-find-out-more').click()
+
+        cy.get('@iframe').find('#myModal').as('modal')
+
+        cy.get('@modal').should(($expectedText) => {
+            const text = $expectedText.text()
+            expect(text).to.include('Welcome to webdriveruniversity.com we sell a wide range of electrical');
+        })
+
+        cy.get('@modal').contains('Close').click()
+    });
+
+})
