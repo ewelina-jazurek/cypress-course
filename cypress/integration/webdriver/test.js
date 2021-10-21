32ea131881ba5e13fa8a6be0dc481768e1e1fe4e
Ewelina <ewelinajazurek@gmail.com>
2021-10-21 15:27:59 +0200

    cy.webdriverUni_ContactForm_Submission

--- cypress/integration/webdriver/test.js
+++ cypress/integration/webdriver/test.js
@@ -8,7 +8,7 @@ describe("Test Contact Us form via WebdriverUni", () => {
             globalThis.data = data;
         })
     })
-    it.only("Should be able to submit a successful submission via contact us form", () => {
+    it("Should be able to submit a successful submission via contact us form", () => {
         // cypress code 
         // cy.visit("https://webdriveruniversity.com/Contact-Us/contactus.html")
         cy.visit("https://webdriveruniversity.com")
@@ -17,14 +17,14 @@ describe("Test Contact Us form via WebdriverUni", () => {
         cy.title().should('include', 'WebDriver | Contact Us')
         cy.url().should('include', 'contactus')
         // cy.get('#contact-us').click({force: true})
-        cy.get('[name ="first_name"]').type(data.first_name)
-        cy.get('[name ="last_name"]').type(data.last_name)
-        cy.get('[name ="email"]').type(data.email)
-        cy.get('[name ="email"]').should('have.attr', 'name', 'email')
-        cy.get('textarea.feedback-input').type("hello")
-        cy.get('[type="submit"]').click();
-        cy.get('h1').should('have.text', 'Thank You for your Message!')
-
+        // cy.get('[name ="first_name"]').type(data.first_name)
+        // cy.get('[name ="last_name"]').type(data.last_name)
+        // cy.get('[name ="email"]').type(data.email)
+        // cy.get('[name ="email"]').should('have.attr', 'name', 'email')
+        // cy.get('textarea.feedback-input').type("hello")
+        // cy.get('[type="submit"]').click();
+        // cy.get('h1').should('have.text', 'Thank You for your Message!')
+        cy.webdriverUni_ContactForm_Submission(data.first_name, data.last_name, data.email, "hello", 'h1', 'Thank You for your Message!');
     });
 
     // .only to keep only 1 test scenario 
@@ -32,10 +32,12 @@ describe("Test Contact Us form via WebdriverUni", () => {
         // cy.visit("https://webdriveruniversity.com/Contact-Us/contactus.html")
         cy.visit("https://webdriveruniversity.com")
         cy.get('#contact-us').invoke('removeAttr', 'target').click({ force: true })
-        cy.get('[name ="first_name"]').type("Tom")
-        cy.get('[name ="last_name"]').type("Smith")
-        cy.get('textarea.feedback-input').type("hello")
-        cy.get('[type="submit"]').click();
-        cy.get('body').contains('Error: all fields are required')
+        // cy.get('[name ="first_name"]').type("Tom")
+        // cy.get('[name ="last_name"]').type("Smith")
+        // cy.get('textarea.feedback-input').type("hello")
+        // cy.get('[type="submit"]').click();
+        // cy.get('body').contains('Error: all fields are required')
+        cy.webdriverUni_ContactForm_Submission(data.first_name, data.last_name, " ", "hello", 'body', 'Error: Invalid email address');
+
     });
 })
