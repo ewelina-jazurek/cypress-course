de8fa113c2a99675f06e49b61b9b66e13e9836b7
Ewelina <ewelinajazurek@gmail.com>
2021-10-21 16:33:00 +0200

    cy.addProductToBasket(element)

---
+++ cypress/integration/automation/add-multiple-items-to-basket.js
@@ -0,0 +1,23 @@
+
+/// <reference types="Cypress" />
+
+describe("Add multiple product to basket", () => {
+
+    before(function () {
+        cy.fixture("products").then(function (data) {
+            globalThis.data = data;
+        });
+    });
+
+    beforeEach(function () {
+        cy.visit("https://automationteststore.com/")
+        cy.get("a[href*='product/category&path=']").contains("Hair Care").click()
+    });
+
+    it("Add specific product to basket", () => {
+        globalThis.data.productName.forEach(function (element) {
+            cy.addProductToBasket(element)
+        });
+
+    })
+})
