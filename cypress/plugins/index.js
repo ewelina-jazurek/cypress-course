ab74e674d58cc1d97c7aca77ec2857e15f66561a
ewelina-jazurek <ewelinajazurek@gmail.com>
2021-10-25 22:12:16 +0200

    config

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -13,33 +13,33 @@
 // the project's config changing)
 
 // promisified fs module
-const fs = require('fs-extra')
-const path = require('path')
+// const fs = require('fs-extra')
+// const path = require('path')
 
-function getConfigurationByFile(file) {
-  const pathToConfigFile = path.resolve('..', 'config', `${file}.json`)
+// function getConfigurationByFile(file) {
+//   const pathToConfigFile = path.resolve('..', 'config', `${file}.json`)
 
-  if(!fs.existsSync(pathToConfigFile)) {
-    console.log('No custom config file found.')
-    return {};
-  }
+//   if(!fs.existsSync(pathToConfigFile)) {
+//     console.log('No custom config file found.')
+//     return {};
+//   }
 
-  return fs.readJson(pathToConfigFile)
-}
-
-// plugins file
-module.exports = (on, config) => {
-  // accept a configFile value or use development by default
-  const  file = config.env.configFile || 'development'
-
-  return getConfigurationByFile(file)
-}
-
-// /**
-//  * @type {Cypress.PluginConfig}
-//  */
-// // eslint-disable-next-line no-unused-vars
-// module.exports = (on, config) => {
-//   // `on` is used to hook into various events Cypress emits
-//   // `config` is the resolved Cypress config
+//   return fs.readJson(pathToConfigFile)
 // }
+
+// // plugins file
+// module.exports = (on, config) => {
+//   // accept a configFile value or use development by default
+//   const  file = config.env.configFile || 'development'
+
+//   return getConfigurationByFile(file)
+// }
+
+/**
+ * @type {Cypress.PluginConfig}
+ */
+// eslint-disable-next-line no-unused-vars
+module.exports = (on, config) => {
+  // `on` is used to hook into various events Cypress emits
+  // `config` is the resolved Cypress config
+}
