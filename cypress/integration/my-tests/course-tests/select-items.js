7047e2f42fcdf8458f528972d8fd5d7802beafc5
ewelina-jazurek <ewelinajazurek@gmail.com>
2021-10-26 11:14:36 +0200

    sync async

--- cypress/integration/my-tests/course-tests/select-items.js
+++ cypress/integration/my-tests/course-tests/select-items.js
@@ -10,7 +10,7 @@ describe("Selecting items", () => {
  .click();
     })
 
-    it.only("add selected product to basket", () => {
+    it("add 1 selected product to basket", () => {
         // cy.get('body > div:nth-child(1) > div:nth-child(3) > div:nth-child(1) > div:nth-child(1) > section:nth-child(5) > div:nth-child(2) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(2)')
         // .click();
             cy.get('body > div:nth-child(1) > div:nth-child(3) > div:nth-child(1) > div:nth-child(1) > section:nth-child(9) > div:nth-child(2) > div:nth-child(1) > div:nth-child(2) > div:nth-child(2) > div:nth-child(2)')
@@ -19,5 +19,20 @@ describe("Selecting items", () => {
         cy.get('.cart').click()
 
            })
-})
 
+           it("add 1 product to basket", () => {
+            cy.get("a[href*='product/category&path=']").contains("Gift Ideas & Sets").click({force: true})
+            cy.get('.prdocutname').contains("Lancome Hypnose Doll Lashes Mascara 4-Piece Gift Set")
+            .click()
+            cy.get('.cart').click()
+            });
+
+            it.only("click on the selected item", () => {
+                cy.get("a[href*='product/category&path=']").contains("Gift Ideas & Sets").click({force: true})
+                cy.get('.fixed_wrapper').find('.prdocutname').eq(2).click()
+                });
+
+        });
+    
+
+        // 
